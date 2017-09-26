20d0435af2c3ccf782c03b25080077618099196a
Atul Varma <varmaa@gmail.com>
2017-09-26 18:41:13 -0400

    attempt to change mount path

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 module.exports = {
+  // https://github.com/18F/federalist-garden-build/blob/staging/build.sh#L85
+  assetPrefix: process.env.BASEURL || '/',
   exportPathMap: function () {
     return {
       '/': { page: '/' },
